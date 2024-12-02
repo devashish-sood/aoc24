3c2fecd623f3363c6a6514ccd59bed0c7fa9f640
Devashish Sood <devashishsood9@gmail.com>
2024-12-01 23:32:20 -0500

    Day 1 Not very pretty, much to learn still lol

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,23 @@
-let () = print_endline "Hello, World!"
+open Core
+
+let file = "lib/inputs/1/actual.txt"
+
+let res_part_1 = let raw_file = In_channel.read_all file
+in 
+raw_file |> Days.Day1.part1;;
+
+
+let res_part_2 = let raw_file = In_channel.read_all file
+in 
+raw_file |> Days.Day1.part2;;
+
+
+
+printf "Result: %d\n" res_part_1;;
+printf "Result: %d\n" res_part_2;;
+
+(* let print_map_as_list map = 
+  let pairs = Days.Day1.IntMap.to_list map in
+  List.iter ~f:(fun p -> Printf.printf "(%d, %d)\n" (fst p) (snd p)) pairs;; *)
+
+(* print_map_as_list res_part_2;; *)
---
+++ lib/days/day1.ml
@@ -0,0 +1,29 @@
+
+let parse input = 
+  let parseline line = 
+    match String.split_on_char ' ' line |> List.filter (fun x -> x <> "") with
+    | [x; y] -> (int_of_string x, int_of_string y)
+    | _ -> failwith "invalid"
+  in
+input |> String.split_on_char '\n' |> List.map parseline;;
+let part1 input =
+  let parsed = parse input in
+    let firsts = List.map (fun x -> fst x) parsed in
+      let seconds = List.map (fun x -> snd x) parsed in
+        List.fold_left (fun acc x -> acc + abs (fst x - snd x)) 0 (List.combine (List.sort compare firsts) (List.sort compare seconds));;
+
+
+module IntMap = Map.Make(Int);;
+let part2 input = 
+  let parsed = parse input in 
+      let keys = List.fold_left (fun acc x -> acc |> IntMap.add x 0) IntMap.empty (List.map (fun x -> fst x) parsed) in
+        let freqs = List.fold_left (fun acc x -> if (IntMap.mem x acc) then IntMap.update x (Option.map (fun x -> x+1)) acc else acc) keys (List.map (fun x -> snd x) parsed) in 
+          List.fold_left (fun acc x -> match IntMap.find_opt x freqs with
+            | None -> acc
+            | (Some y) -> (x * y) + acc
+            )
+            0 
+            (List.map (fun x -> fst x) parsed) 
+          (* List.fold_left (fun acc x -> (fst x * snd x) + acc) 0 (IntMap.to_list freqs) *)
+
+
