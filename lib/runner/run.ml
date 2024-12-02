debbc674039769b70e8c4a6c0dd6ba9654f1e3c8
Devashish Sood <devashishsood9@gmail.com>
2024-12-02 11:19:20 -0500

    formatting

--- lib/runner/run.ml
+++ lib/runner/run.ml
@@ -1,3 +1,3 @@
-let run p1 p2 input = 
+let run p1 p2 input =
   Printf.printf "Part 1 Result: %d\n" (p1 input);
-  Printf.printf "Part 2 Result: %d\n" (p2 input);
+  Printf.printf "Part 2 Result: %d\n" (p2 input)
