374b419a02d647242786036aa2ef5f3135d7684b
Devashish Sood <devashishsood9@gmail.com>
2024-12-04 20:38:31 -0500

    Day 3 These list abstractions are beginning to get to me

--- bin/main.ml
+++ bin/main.ml
@@ -1,18 +1,17 @@
 open Core
 
-let file = "inputs/2/actual.txt"
+let file = "inputs/3/actual.txt"
 
 let res_part_1 =
   let raw_file = In_channel.read_all file in
-  raw_file |> Days.Day2.part1;;
+  raw_file |> Days.Day3.part1
+;;
 
-printf "Result: %d\n" res_part_1;;
+printf "Result: %d\n" res_part_1
 
 let res_part_2 =
   let raw_file = In_channel.read_all file in
-  raw_file |> Days.Day2.part2
+  raw_file |> Days.Day3.part2
 ;;
 
 printf "Result: %d\n" res_part_2;;
-
-(* let () = List.iter ~f:(fun x -> Printf.printf "%b " x) res_part_2; *)
---
+++ lib/days/day3.ml
@@ -0,0 +1,53 @@
+(* regex for p1 "mul([0-9]+,[0-9]+)"  ocaml specific *)
+let parse input =
+  (* let parseline line =
+       List.map int_of_string
+         (String.split_on_char ' ' line |> List.filter (fun x -> x <> ""))
+     in *)
+  input |> String.split_on_char '\n'
+
+let mul_pattern = Str.regexp "mul([0-9]+,[0-9]+)"
+let num_pattern = Str.regexp "[0-9]+"
+let grab regex s = Str.full_split regex s
+
+let get_nums s =
+  List.filter_map
+    (fun r ->
+      match r with Str.Text _ -> None | Str.Delim x -> Some (int_of_string x))
+    (grab num_pattern s)
+
+let multiply vals = List.fold_left (fun res v -> res * v) 1 vals
+
+let part1 input =
+  input |> parse
+  |> List.map (grab mul_pattern)
+  |> List.map
+       (List.filter_map (fun y ->
+            match y with Str.Text _ -> None | Str.Delim x -> Some x))
+  |> List.flatten |> List.map get_nums
+  |> List.fold_left (fun acc vals -> acc + multiply vals) 0
+
+let conditional_mul_pattern = Str.regexp "mul([0-9]+,[0-9]+)\\|do()\\|don't()"
+
+let rec apply_conditions apply acc statement =
+  match statement with
+  | [] -> acc
+  | x :: v -> (
+      match x with
+      | "do()" -> apply_conditions true acc v
+      | "don't()" -> apply_conditions false acc v
+      | nums ->
+          if apply then
+            apply_conditions apply (acc + multiply (get_nums nums)) v
+          else apply_conditions apply acc v)
+
+let part2 input =
+  input |> parse
+  |> List.map (grab conditional_mul_pattern)
+  |> List.map
+       (List.filter_map (fun y ->
+            match y with Str.Text _ -> None | Str.Delim x -> Some x))
+  |> List.flatten
+  |> List.filter (fun s ->
+         s = "do()" || s = "don't()" || Str.string_match mul_pattern s 0)
+  |> apply_conditions true 0
